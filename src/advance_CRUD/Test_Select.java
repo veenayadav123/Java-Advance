37d791222b386f3c4b46217e65ae8a07dc83574d
veenayadav123 <yveena24yadav@gmail.com>
2022-04-04 19:12:15 +0530

    Success

--- src/advance_CRUD/Test_Select.java
+++ src/advance_CRUD/Test_Select.java
@@ -1,10 +1,9 @@
 package advance_CRUD;
 
 import java.sql.DriverManager;
+import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
-
-import com.mysql.jdbc.Connection;
 
 public class Test_Select {
 
@@ -17,20 +16,20 @@ public class Test_Select {
 
 		Class.forName("com.mysql.jdbc.Driver");
 		
-		Connection conn = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/rays", "root", "root");
+		Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/rays", "root", "root");
 
 		Statement stmt = conn.createStatement();
 
 		ResultSet rs = stmt.executeQuery("SELECT * FROM MARKSHEET");
 
 		while (rs.next()) {
-			System.out.print("\t" + rs.getString(1));
+			System.out.print("\t" + rs.getInt(1));
 			System.out.print("\t" + rs.getString(2));
 			System.out.print("\t" + rs.getString(3));
 			System.out.print("\t" + rs.getString(4));
-			System.out.print("\t" + rs.getString(5));
-			System.out.print("\t" + rs.getString(6));
-			System.out.println("\t" + rs.getString(7));
+			System.out.print("\t" + rs.getInt(5));
+			System.out.print("\t" + rs.getInt(6));
+			System.out.println("\t" + rs.getInt(7));
 		}
 		stmt.close();
 		conn.close();
